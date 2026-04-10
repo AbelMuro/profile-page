19fe334846e8cdcbb9390767aff8a0ee7f9a5a7b
AbelMuro <abelmuro93@gmail.com>
2026-04-10 16:52:53 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import SideBar from './Components/SideBar';
+
+/* 
+    this is where i left off, i need to work on the SideBar component
+*/
+
+function App() {
+    return(
+        <>
+            <SideBar/>
+        </>
+    )
+};
+
+export default App;
---
+++ src/Components/SideBar/SideBar.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import * as styles from './styles.module.css';
+
+function SideBar() {
+    return (<aside> Hello sidebar</aside>)
+}
+
+export default SideBar;
